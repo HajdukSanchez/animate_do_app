da50bd8c6f4ebc8bd176ee5f7991ffd3f965427f
jasanchez3 <jasanchez3@latam.stefanini.com>
2022-05-03 21:49:39 -0500

    🚧 [ADD] Basic Animations

--- lib/src/pages/home_page/home_page.dart
+++ lib/src/pages/home_page/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animate_do/animate_do.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,30 +11,51 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Animate Do"),
+        title:
+            FadeIn(duration: const Duration(milliseconds: 2000), child: const Text("Animate Do")),
         actions: [
-          IconButton(onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.twitter)),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.navigate_next_rounded)),
+          IconButton(onPressed: () {}, icon: const Icon(FontAwesomeIcons.twitter)),
+          SlideInLeft(
+            from: 100,
+            child: IconButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      CupertinoPageRoute(builder: (BuildContext context) => const HomePage()));
+                },
+                icon: const Icon(Icons.navigate_next_rounded)),
+          ),
         ],
       ),
-      floatingActionButton:
-          FloatingActionButton(onPressed: () {}, child: const FaIcon(FontAwesomeIcons.play)),
+      floatingActionButton: ElasticInRight(
+          child: FloatingActionButton(onPressed: () {}, child: const Icon(FontAwesomeIcons.play))),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Icon(
-              Icons.new_releases_rounded,
-              size: 40,
-              color: Colors.blue,
+            ElasticIn(
+              delay: const Duration(milliseconds: 1500),
+              child: const Icon(
+                Icons.new_releases_rounded,
+                size: 40,
+                color: Colors.blue,
+              ),
             ),
-            const Text("Title", style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200)),
-            const Text("SubTitle", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              width: 220,
-              height: 2,
-              color: Colors.blue,
+            FadeInDown(
+                delay: const Duration(milliseconds: 200),
+                child: const Text("Title",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200))),
+            FadeInDown(
+                delay: const Duration(milliseconds: 800),
+                child: const Text("SubTitle",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400))),
+            FadeInLeft(
+              delay: const Duration(milliseconds: 1100),
+              child: Container(
+                margin: const EdgeInsets.only(top: 10),
+                width: 220,
+                height: 2,
+                color: Colors.blue,
+              ),
             )
           ],
         ),
